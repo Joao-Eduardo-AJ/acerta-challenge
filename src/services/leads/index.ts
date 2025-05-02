e25df47d12d90357644f33772759fbc90eabd9b5
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2025-05-02 01:45:37 -0300

    feat: config deploy files

--- src/services/leads/index.ts
+++ src/services/leads/index.ts
@@ -2,7 +2,7 @@ import { api } from '@src/api'
 import { ILead, ILeadsResponse } from '@src/types/leads'
 
 async function listUsers(page: number, cpf?: string, name?: string) {
-  const { data } = await api.get<Promise<ILeadsResponse>>('/leads', {
+  const { data } = await api.get<Promise<ILeadsResponse>>('/api', {
     params: {
       _page: page,
       name,
@@ -13,11 +13,11 @@ async function listUsers(page: number, cpf?: string, name?: string) {
   return data
 }
 
-const createUser = async (data: ILead) => await api.post('/leads', data)
+const createUser = async (data: ILead) => await api.post('/api', data)
 
 const updateUser = async (data: ILead, id: string) =>
-  await api.put(`/leads/${id}`, data)
+  await api.put(`/api/${id}`, data)
 
-const deleteUser = async (id: string) => await api.delete(`/leads/${id}`)
+const deleteUser = async (id: string) => await api.delete(`/api/${id}`)
 
 export { createUser, listUsers, updateUser, deleteUser }
